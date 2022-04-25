7803855523c54b99ae52e53c35bcccb04877b4c5
BrandonSalazar17 <93247096+BrandonSalazar17@users.noreply.github.com>
2022-04-25 16:20:42 -0500

    first commit

---
+++ Pedir-botones.js
@@ -0,0 +1,10 @@
+<script language="JavaScript1.2">
+        function UpdateCartQuantity() {
+            document.cart_quantity.submit();
+        }
+        
+        function changeQuantity(qty) {
+            document.cart_quantity['quantity'].value = Number(document.cart_quantity['quantity'].value)+Number(qty);
+            UpdateCartQuantity();
+        }
+    </script>
